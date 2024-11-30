bc1b861e9aaeb63e669aa3f077fe95b4ae319eed
lmReef <ReefMatson@gmail.com>
2024-11-30 18:57:05 +1300

    feat: add chance to find ferrystones on item pickup events

--- ferrystones_vanilla+.lua
+++ ferrystones_vanilla+.lua
@@ -1,5 +1,4 @@
 local ItemManager = sdk.get_managed_singleton("app.ItemManager")
--- local FerrystoneFlowController = sdk.get_managed_singleton("app.FerrystoneFlowController")
 local CharacterManager = sdk.get_managed_singleton("app.CharacterManager")
 
 local PermanentPortcrystals = {
@@ -11,10 +10,10 @@ local PermanentPortcrystals = {
 	-- "", -- Seafloor Shrine
 }
 
-local function get_player_position()
-	local position = nil
+math.randomseed(os.time())
 
-	local character = CharacterManager:get_field("<ManualPlayer>k__BackingField")
+local function get_player_position()
+	local character = CharacterManager:get_ManualPlayer()
 
 	if character then
 		local gameObject = character:get_GameObject()
@@ -23,13 +22,12 @@ local function get_player_position()
 			local transform = gameObject:get_Transform()
 
 			if transform then
-				local universalPosition = transform:get_UniversalPosition()
-				position = universalPosition:ToString()
+				return transform:get_UniversalPosition():ToString()
 			end
 		end
 	end
 
-	return position
+	return nil
 end
 
 local function player_pos_is_perm_portcrystal()
@@ -46,14 +44,43 @@ local function add_ferrystone(is_notice)
 	ItemManager:call("getItem", 80, 1, 2891076981, is_notice, false, false, 0)
 end
 
+-- refund ferrystones used on permanent portcrystals
 sdk.hook(
 	sdk.find_type_definition("app.FerrystoneFlowController"):get_method("teleport"),
 	function()
 	end,
 	function()
-		log.debug("Player: " .. get_player_position())
+		-- log.debug("Player: " .. get_player_position())
 		if player_pos_is_perm_portcrystal() then
 			add_ferrystone(false)
 		end
 	end
+)
+
+sdk.hook(
+	sdk.find_type_definition("app.ItemManager"):get_method(
+		"getItem(System.Int32, System.Int32, app.Character, System.Boolean, System.Boolean, System.Boolean, app.ItemManager.GetItemEventType, System.Boolean, System.Boolean)"),
+	function(args)
+		local type_info = {
+			-- [1] = { name = "None", chance = 0.0 },
+			[2] = { name = "Gather", chance = 0.03 },
+			[4] = { name = "TreasureBox", chance = 0.20 },
+			[8] = { name = "Talk", chance = 0.15 },
+			[16] = { name = "DeadEnemy", chance = 0.05 }
+		}
+
+		if args[9] ~= nil then
+			local source = sdk.to_int64(args[9])
+			-- log.debug(source)
+			-- log.debug(type_info[source].name)
+
+			if source ~= nil and type_info[source] ~= nil then
+				if math.random() <= type_info[source].chance then
+					-- TODO: currently this just adds to arisen's inv even if a pawn procs it; add to pawn inv?
+					add_ferrystone(true)
+				end
+			end
+		end
+	end,
+	nil
 )
